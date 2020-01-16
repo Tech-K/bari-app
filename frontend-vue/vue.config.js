8e62f137813e3b9858e3ae6ab5fcfa1af528e86b
Yrving Blanco <ciscomi45@gmail.com>
2020-01-16 10:58:50 -0400

    Vue in my Bari-App

---
+++ frontend-vue/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: 'Bari-App',
+    themeColor: '#4A90E2',
+    msTileColor: '#4A90E2',
+    manifestOptions: {
+      background_color: '#4A90E2'
+    }
+  }
+}
